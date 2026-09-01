f7a750233ccac33185924c04bac0d4f67b9e12e5
snexis <sonu26bolg@gmail.com>
2026-09-01 13:26:44 +0530

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -1,3 +1,7 @@
+exports.config = {
+  timeout: 26
+};
+
 exports.handler = async function (event, context) {
   const headers = {
     "Access-Control-Allow-Origin": "*",
@@ -34,7 +38,7 @@ exports.handler = async function (event, context) {
     }
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent?key=${apiKey}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=${apiKey}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
